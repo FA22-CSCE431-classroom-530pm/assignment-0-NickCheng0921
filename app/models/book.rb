a32522f014fb1e4a748e66ba3bf2b44585f0bce8
NickCheng0921 <nickshacheng.0921@gmail.com>
2022-08-31 15:04:20 -0500

    Unit test for book title

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ApplicationRecord
+    validates :title, presence: true #book must have a non empty title
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -4,4 +4,8 @@ class BookTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "book_must_have_title" do
+    book = Book.new
+    assert_not book.save, "Incorrectly saved new book without title"
+  end
 end
