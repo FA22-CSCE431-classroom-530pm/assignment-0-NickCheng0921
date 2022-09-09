28e4fbcab79dca57eeedeea6ab9e643be54c8364
NickCheng0921 <nickshacheng.0921@gmail.com>
2022-09-09 14:15:10 -0500

    Added test

---
+++ spec/unit/unit_spec.rb
@@ -0,0 +1,32 @@
+# location: spec/unit/unit_spec.rb
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'harry potter', author: 'nick cheng', price: 1.0, published_date: DateTime.now.to_time)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  #it 'is not valid without a datetime' do
+  #  subject.published_date = nil
+  #  expect(subject).not_to be_valid
+  #end
+end
